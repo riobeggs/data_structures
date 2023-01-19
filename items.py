88c658bf85e861cd7e78b4fc84c5f748daf9557d
Rio <riobeggs1@gmail.com>
2023-01-20 01:17:43 +1300

    Added iteration method

--- items.py
+++ items.py
@@ -7,8 +7,35 @@ class Items:
     # Can return specific item
     # Can sort items (optional)
     _head = None
+    _current = None
 
-    # def __iter__()
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # If first call return head
+        # Get next item if there is one
+        # Otherwise raise StopIteration
+
+        if self.is_empty():
+            raise StopIteration
+
+        # If start of list 
+        if self._current is None:
+            self._current = self._head
+        # If next item
+        elif self._current.has_next_item():
+            self._current = self._current.get_next_item()
+        # If end of list
+        else:
+            # self._current = None
+            raise StopIteration 
+
+        return self._current
+
+        
+        
+
 
     # def get_item_by_index()
 
@@ -23,3 +50,4 @@ class Items:
 
     def is_empty(self) -> bool:
         return self._head is None
+
