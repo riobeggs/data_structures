78158c810b2d8b8b8be6ab9cea8eb45e6f409d03
Rio <riobeggs1@gmail.com>
2023-01-20 14:31:43 +1300

    * Added storing of tail item * Added method for getting an item by index

--- items.py
+++ items.py
@@ -7,7 +7,9 @@ class Items:
     # Can return specific item
     # Can sort items (optional)
     _head = None
+    _tail = None
     _current = None
+    _index = 0
 
     def __iter__(self):
         return self
@@ -26,14 +28,21 @@ class Items:
         # If next item
         elif self._current.has_next_item():
             self._current = self._current.get_next_item()
+            self._index += 1
         # If end of list
         else:
             self._current = None
+            self._index += 1
             raise StopIteration
 
         return self._current
 
-    # def get_item_by_index()
+    def get_item_by_index(self, index: int):
+        for _ in self:
+            if self._index == index:
+                return self._current
+
+        raise IndexError(f"Index {index} is out of range")
 
     def store_head(self, head: SingleItem):
         if not isinstance(head, SingleItem):
@@ -43,6 +52,15 @@ class Items:
 
     def retrieve_head(self) -> SingleItem:
         return self._head
+
+    def store_tail(self, tail: SingleItem):
+        if not isinstance(tail, SingleItem):
+            raise Exception(f"{tail} is not an instance of a SingleItem")
+
+        self._tail = tail
+
+    def retrieve_tail(self) -> SingleItem:
+        return self._tail
 
     def is_empty(self) -> bool:
         return self._head is None
