13b8b1ba321a89f0a9d728e317f6335d7a54ae7f
Rio <riobeggs1@gmail.com>
2023-01-24 10:40:55 +1300

    Item class refactoring * Renamed class to Item. * Created an init method. * Added value setter/getter. * Added next item setter/getter.

--- item.py
+++ item.py
@@ -1,26 +1,28 @@
-class SingleItem:
+class Item:
     # Single item needs to know next item.
     # Returns own value
-    _value = None
-    _next_item = None
+    def __init__(self) -> None:
+        self._value = None
+        self._next_item = None
 
     def __str__(self) -> str:
-        # return f"SingleItem({self._value})"
         return str(self._value)
 
-    def add(self, value):
-        self._value = value
-
+    @property
     def value(self):
         return self._value
 
-    def set_next_item(self, next_item: "SingleItem"):
-        if not isinstance(next_item, SingleItem):
-            raise Exception(f"{next_item} is not an instance of a SingleItem")
-        self._next_item = next_item
+    @value.setter
+    def value(self, value) -> None:
+        self._value = value
 
-    def next_item(self) -> "SingleItem":
+    @property
+    def next_item(self):
         return self._next_item
+
+    @next_item.setter
+    def next_item(self, next_item) -> None:
+        self._next_item = next_item
 
     def has_next_item(self) -> bool:
         # if self.get_next_item == None:
