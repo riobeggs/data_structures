324eacdc43b0831da88536a9a6671eec75554fde
Rio <riobeggs1@gmail.com>
2023-01-24 20:45:37 +1300

    Added future annotations/ return types/ hinting.

--- item.py
+++ item.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Any
 
 
@@ -31,14 +33,14 @@ class Item:
         self._value = value
 
     @property
-    def next_item(self) -> "Item" | None:
+    def next_item(self) -> Item | None:
         """
         Retrieves the items next item.
         """
         return self._next_item
 
     @next_item.setter
-    def next_item(self, next_item: "Item" | None) -> None:
+    def next_item(self, next_item: Item | None) -> None:
         """
         Sets the next item for the item.
         """
