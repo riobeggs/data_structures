fc46f4acf299890e1c4cc74e6ca50eb144b79fb6
Rio <riobeggs1@gmail.com>
2023-02-07 23:02:08 +1300

    Finished tests for item list class init method.

--- tests/item_list_test.py
+++ tests/item_list_test.py
@@ -4,32 +4,28 @@ from parameterized import parameterized
 from assets.item_list import List as MyList
 
 
-class TestSorting(unittest.TestCase):
+class Tests(unittest.TestCase):
     """Stop complaining about doc strings in tests."""
 
-    @parameterized.expand([([2, 5, 1, 4, 3],), ([1],), (None,)])
-    def test_initialize(self, input_data: list[int]):
+    @parameterized.expand([([2, 5, 1, 4, 3], 2, 3), ([1], 1, 1), ([None], None, None)])
+    def test_initialize(
+        self,
+        input_data: list[int] | None,
+        expected_head: int | None,
+        expected_tail: int | None,
+    ):
         """Tests we can initialize a wide range of inputs."""
-        item_list: List = MyList(input_data)
-        # item_list.add([2, 5, 1, 4, 3])
 
-        self.assertEqual(item_list.tail.value, 3)
-        self.assertEqual(item_list.head.value, 2)
+        # Act
+        item_list = MyList(input_data)
 
-        for index, item in enumerate(input_data):
-            pass
+        # Arrange
+        expected_length = len(input_data)
+        head = input_data[0]
+        tail = input_data[-1]
+        length = len(item_list)
 
-        test = [item.value for item in item_list]
-        expected = input_data
-        self.assertEqual(test, expected)
-
-        # # Arrange
-        # expected: list[int] = [1, 2, 3, 4, 5]
-
-        # # Act
-        # item_list.sort()
-
-        # # Assert
-        # self.assertEqual(item_list, expected)
-
-        # # print(item_list)
+        # Assert
+        self.assertEqual(head, expected_head)
+        self.assertEqual(tail, expected_tail)
+        self.assertEqual(length, expected_length)
